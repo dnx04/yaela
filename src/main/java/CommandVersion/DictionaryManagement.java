e0f0fd4750a4064a16aba880e8a971453d21febe
huynq2k4 <huynq.contact@gmail.com>
2023-11-13 16:24:36 +0700

    Build command version

--- src/main/java/CommandVersion/DictionaryManagement.java
+++ src/main/java/CommandVersion/DictionaryManagement.java
@@ -4,7 +4,43 @@ import java.io.*;
 import java.util.Scanner;
 import java.util.Collections;
 
-public class DictionaryManagement extends Dictionary {
+public class DictionaryManagement extends Dictionary{
+
+    /** Find the position to insert in array. */
+    private static int findInsertIndex(Word w) {
+        int start = 0;
+        int end = words.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int cmp = words.get(mid).getWordTarget().compareTo(w.getWordTarget());
+            if (cmp == 0) {
+                return -1;
+            } else if (cmp < 0) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return end + 1;
+    }
+
+    /** Find the position of a word in an array, return -1 if not exist. */
+    private static int binarySearch(Word w) {
+        int start = 0;
+        int end = words.size() - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            int cmp = words.get(mid).getWordTarget().compareTo(w.getWordTarget());
+            if (cmp == 0) {
+                return mid;
+            } else if (cmp < 0) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
 
     public static void insertFromCommandline() {
         Scanner s = new Scanner(System.in);
@@ -20,119 +56,124 @@ public class DictionaryManagement extends Dictionary {
 
     public static void insertFromFile() {
 
-        try {
-            Scanner s = new Scanner(new File("./src/dictionaries.txt"));
-            String line = s.nextLine();
-            BufferedReader r = new BufferedReader(new FileReader("./src/dictionaries.txt"));
-
+        try{
+            BufferedReader r = new BufferedReader(new FileReader("./yaela/src/main/java/CommandVersion/dictionaries.txt"));
+            String line;
             while ((line = r.readLine()) != null) {
                 String[] parts = line.split("\t", 2);
-                if (parts.length >= 2) {
+                if(parts.length >= 2){
                     Word newWord = new Word(parts[0].trim(), parts[1].trim());
-                    words.add(newWord);
+                    
+                    int insertIndex = binarySearch(newWord);
+                    if (insertIndex != -1) {
+                        words.get(insertIndex).setWordExplain(newWord.getWordExplain());
+                    } else {
+                        insertIndex = findInsertIndex(newWord);
+                        words.add(insertIndex, newWord);
+                    }
                 }
             }
             Collections.sort(words);//Sort words in ascending order
             r.close();
+            System.out.println("Imported successfully!");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void dictionaryLookup() {
+    public static void dictionaryLookup(){
         Scanner s = new Scanner(System.in);
         System.out.println("Enter the word you want to look up: ");
         String lookUpWord = s.nextLine();
+        Word word = new Word(lookUpWord, null);
 
-        //NEED TO UPDATE THIS PART BY USING BINARY SEARCH 
-        for (Word w : words) {
-            if (w.getWordTarget().equals(lookUpWord)) {
-                System.out.println(w.getWordExplain());
-                return;
-            }
+
+        int index = binarySearch(word);
+        if (index != -1) {
+            System.out.println(words.get(index).getWordExplain());
+        } else {
+            System.out.println("Word not found!");
         }
-        System.out.println("CommandVersion.Word not found!");
-        //
+
 
 
     }
 
-    public static void addWord() {
+    public static void addWord(){
 
         Scanner s = new Scanner(System.in);
 
         System.out.println("Enter the word you want to add: ");
         String newWordTarget = s.nextLine();
 
+        
+
         System.out.println("Enter the word explanation: ");
         String newWordExplain = s.nextLine();
 
         Word newWord = new Word(newWordTarget, newWordExplain);
 
-        //NEED TO UPDATE THIS PART BY FINDING THE POSITION TO ADD WORD
-        words.add(newWord);
-        Collections.sort(words);
-        //
+        int insertIndex = findInsertIndex(newWord);
 
-        System.out.println("CommandVersion.Word added!");
+        if (insertIndex == -1) {
+            System.out.println("Cannot add word: word has existed!");
+        } else {
+            words.add(insertIndex, newWord);
+            System.out.println("Word added!");
+        }
+        
 
+
+        
+        
     }
 
-    public static void updateWord() {
+    public static void updateWord(){
 
         Scanner s = new Scanner(System.in);
 
         System.out.println("Enter the word you want to update: ");
         String updateWordTarget = s.nextLine();
+        Word word = new Word(updateWordTarget, null);
 
-        //NEED TO UPDATE THIS PART USING BINARY SEARCH
-        for (Word w : words) {
-            if (w.getWordTarget().equals(updateWordTarget)) {
+        int index = binarySearch(word);
+        if (index != -1) {
+            System.out.println("Enter the update explanation: ");
+            String updateWordExplain = s.nextLine();
 
-                System.out.println("Enter the update explanation: ");
-                String updateWordExplain = s.nextLine();
+            words.get(index).setWordExplain(updateWordExplain);
 
-                w.setWordExplain(updateWordExplain);
-                System.out.println("CommandVersion.Word updated!");
-
-                return;
-            }
+            System.out.println("Word updated!");
+        } else {
+            System.out.println("Word not found!");
         }
-        System.out.println("CommandVersion.Word not found!");
-        //
+
 
     }
 
-    public static void deleteWord() {
+    public static void deleteWord(){
 
         Scanner s = new Scanner(System.in);
 
         System.out.println("Enter the word you want to delete: ");
         String deleteWordTarget = s.nextLine();
+        Word word = new Word(deleteWordTarget, null);
 
-        //NEED TO UPDATE THIS PART USING BINARY SEARCH
-        for (Word w : words) {
-            if (w.getWordTarget().equals(deleteWordTarget)) {
-
-                words.remove(w);
-                System.out.println("CommandVersion.Word deleted!");
-
-                s.close();
-                return;
-            }
+        int index = binarySearch(word);
+        if (index != -1) {
+            words.remove(index);
+            System.out.println("Word deleted!");
+        } else {
+            System.out.println("Word doesn't exist!");
         }
-        System.out.println("CommandVersion.Word doesn't exist!");
-        //
-
-        //s.close();
     }
 
     public static void dictionaryExportToFile() {
-        File f = new File("./src/dictionaries.txt");
+        File f = new File("./yaela/src/main/java/CommandVersion/dictionaries.txt");
 
         try {
             FileOutputStream output = new FileOutputStream(f);
-            for (Word w : words) {
+            for(Word w: words){
                 String tmp = w.getWordTarget() + "\t" + w.getWordExplain() + "\n";
                 output.write(tmp.getBytes());
             }
--- src/main/java/CommandVersion/Word.java
+++ src/main/java/CommandVersion/Word.java
@@ -1,32 +1,32 @@
 package CommandVersion;
 
-public class Word implements Comparable<Word> {
+public class Word implements Comparable<Word>{
     private String word_target;
     private String word_explain;
 
-    Word(String word_target, String word_explain) {
-        this.word_target = word_target;
-        this.word_explain = word_explain;
-    }
-
     String getWordTarget() {
         return word_target;
-    }
-
-    void setWordTarget(String word_target) {
-        this.word_target = word_target;
     }
 
     String getWordExplain() {
         return word_explain;
     }
 
+    void setWordTarget(String word_target) {
+        this.word_target = word_target;
+    }
+
     void setWordExplain(String word_explain) {
         this.word_explain = word_explain;
     }
 
+    Word(String word_target, String word_explain) {
+        this.word_target = word_target;
+        this.word_explain = word_explain;
+    }
+
     @Override
-    public int compareTo(Word w) {
+    public int compareTo(Word w){
         return this.word_target.compareTo(w.getWordTarget());
     }
 }
