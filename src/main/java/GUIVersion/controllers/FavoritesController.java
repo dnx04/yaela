3c7ed1ec87c45ad79c25e4dbbd817eefc91231b9
huynq2k4 <huynq.contact@gmail.com>
2023-12-02 08:57:00 +0700

    Modify favorites

--- src/main/java/GUIVersion/controllers/FavoritesController.java
+++ src/main/java/GUIVersion/controllers/FavoritesController.java
@@ -1,5 +1,16 @@
 package GUIVersion.controllers;
 
-public class FavoritesController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class FavoritesController implements Initializable {
+
+
     
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
